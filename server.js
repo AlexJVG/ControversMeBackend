88c9fdfffa6f11d80401a58352669a3176286c2a
Arjun Patrawala <ajpat1234@gmail.com>
2018-10-20 16:27:01 -0700

    a

--- server.js
+++ server.js
@@ -43,17 +43,4 @@ io.on('connection', socket => {
 	socket.on("leave", function(id){
 		socket.leave(id);
 	});
-
-// 	socket.on('disconnect', function(){
-//     io.emit('users-changed', {user: socket.nickname, event: 'left'});   
-//   });
- 
-//   socket.on('set-nickname', (nickname) => {
-//     socket.nickname = nickname;
-//     io.emit('users-changed', {user: nickname, event: 'joined'});    
-//   });
-  
-//   socket.on('add-message', (message) => {
-//     io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
-//   });
 });
