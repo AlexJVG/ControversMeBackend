f56bea895c1c66b33ee447dc326d420e5c31bf4d
Arjun Patrawala <ajpat1234@gmail.com>
2018-10-20 11:19:45 -0700

    hi

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 
 
+
+
 app.get('/', (req, res) => {
 
 	res.send('hello');
 
 });
+
+app.get('/egor/:hello', (req, res) => {
+
+	res.send(req.params.hello);
+
+});
+
+app.listen(8080);
