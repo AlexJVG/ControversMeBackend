d62c13dfec70e831d03ee90444cade519520e727
Arjun Patrawala <ajpat1234@gmail.com>
2018-10-20 13:35:58 -0700

    hi

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ const path = require('path');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-const database = new (require('./lib/Database.js'));
-const responses = require('./lib/responses.js');
-
 const cors = require('cors');
-
-app.use('/', express.static(path.join(__dirname, 'public')));
-
 app.use(express.json());
 
 app.options('*', cors());
@@ -22,35 +16,8 @@ app.use(cors({
 	}
 }));
 
-app.post('/api/create-account', (req, res) => {
-
-	const firstName = req.body.first_name;
-	const lastName = req.body.last_name;
-	const username = req.body.username;
-	const email = req.body.email;
-	const bio = req.body.bio;
-	const password = req.body.password;
-
-	console.log(req.body);
-
-	if (firstName && lastName && username && bio && password) {
-		if (database.addNewUser(firstName, lastName, username, email, bio, password)) {
-			res.send(responses.success());
-		} else {
-			res.send(responses.error('user_already_exists'));
-		}
-
-	} else {
-		res.send(responses.error('bad_data'));
-	}
-	
-});
-
-app.get('/egor/:hello', (req, res) => {
-
-	res.send(req.params.hello);
-
-});
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/api', require('./lib/routes/api.js'));
 
 app.get('*', (req, res) => {
 	res.send(responses.error('404 Not Found'));
