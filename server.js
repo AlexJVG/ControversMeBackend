eecb83d4c64ee8dfeece804f810e8f2c807b9ff8
Egor Cherkashin <egor.pavlovski@gmail.com>
2018-10-20 12:22:43 -0700

    Egot shit

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const Database = require('./lib/Database.js');
+const path = require('path')
+//const http = require('http').Server(app)
+const io = require('socket.io');
+const public = path.join(__dirname, 'public')
 
+app.use("/", express.static(public));
 
 
 app.get('/', (req, res) => {
@@ -13,6 +18,12 @@ app.get('/', (req, res) => {
 app.get('/egor/:hello', (req, res) => {
 
 	res.send(req.params.hello);
+
+});
+
+app.get('/eggtest2', (req, res) => {
+	res.sendFile(path.join(public, 'basic.html'));
+	
 
 });
 
