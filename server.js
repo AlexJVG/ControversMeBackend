97047e063eaf76c28bec1a809d9078e25fb31870
Alex Villablanca <1alexvillablanca@gmail.com>
2018-10-20 19:45:23 -0700

    asjklDhflkasdjf

--- server.js
+++ server.js
@@ -21,21 +21,21 @@ app.use(cors({
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/api', require('./lib/routes/api.js').router);
 
-// io.on('connection', require('./lib/routes/api.js').socketResponse);
-
 io.on('connection', (socket) => {
-	socket.on('disconnect', function(){
-    io.emit('users-changed', {user: socket.nickname, event: 'left'});   
+	socket.on('leave-room', function(){
+	socket.leave(socket.room);
+    socket.to(socket.room).emit('users-changed', {user: socket.nickname, event: 'left'});   
   });
- 
-  socket.on('set-nickname', (nickname) => {
-    socket.nickname = nickname;
-    io.emit('users-changed', {user: nickname, event: 'joined'});    
-  });
-  
   socket.on('add-message', (message) => {
-    io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
+  	socket.to(socket.room).emit('message', {text: message.text, from: socket.nickname});
+  	socket.emit('message', {text: message.text, from: socket.nickname});
   });
+  socket.on('join-room',(val) => {
+  	socket.join(val.room);
+  	socket.nickname = val.nickname;
+  	socket.room = val.room;
+    socket.to(socket.room).emit('users-changed', {user: val.nickname, event: 'joined'});
+  })
 });
 
 app.all('*', (req, res) => {
