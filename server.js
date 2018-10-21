3ef61f4c14c89afa8b0c825edd99140b8711f886
Alex Villablanca <1alexvillablanca@gmail.com>
2018-10-20 18:16:56 -0700

    New code for socket

--- server.js
+++ server.js
@@ -21,7 +21,22 @@ app.use(cors({
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/api', require('./lib/routes/api.js').router);
 
-io.on('connection', require('./lib/routes/api.js').socketResponse);
+// io.on('connection', require('./lib/routes/api.js').socketResponse);
+
+io.on('connection', (socket) => {
+	socket.on('disconnect', function(){
+    io.emit('users-changed', {user: socket.nickname, event: 'left'});   
+  });
+ 
+  socket.on('set-nickname', (nickname) => {
+    socket.nickname = nickname;
+    io.emit('users-changed', {user: nickname, event: 'joined'});    
+  });
+  
+  socket.on('add-message', (message) => {
+    io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
+  });
+});
 
 app.all('*', (req, res) => {
 	res.send(responses.error(404));
