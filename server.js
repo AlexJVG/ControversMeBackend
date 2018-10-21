d6331b08ea12f83fa523a681abbf5cfce21793a8
Arjun Patrawala <ajpat1234@gmail.com>
2018-10-20 19:52:41 -0700

    a

--- server.js
+++ server.js
@@ -21,22 +21,7 @@ app.use(cors({
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/api', require('./lib/routes/api.js').router);
 
-io.on('connection', (socket) => {
-	socket.on('leave-room', function(){
-	socket.leave(socket.room);
-    socket.to(socket.room).emit('users-changed', {user: socket.nickname, event: 'left'});   
-  });
-  socket.on('add-message', (message) => {
-  	socket.to(socket.room).emit('message', {text: message.text, from: socket.nickname});
-  	socket.emit('message', {text: message.text, from: socket.nickname});
-  });
-  socket.on('join-room',(val) => {
-  	socket.join(val.room);
-  	socket.nickname = val.nickname;
-  	socket.room = val.room;
-    socket.to(socket.room).emit('users-changed', {user: val.nickname, event: 'joined'});
-  })
-});
+io.on('connection', require('./lib/routes/api.js').socketResponse);
 
 app.all('*', (req, res) => {
 	res.send(responses.error(404));
